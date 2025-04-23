50810cc60042e908854b0dbe8b4d0d8cfea443e6
Md Enamul Haque <enamul807849@gmail.com>
2025-04-23 10:03:46 +0600

    changed

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://kebd-crm.vercel.app','http://localhost:7002','https://kebd.mmadinah.com'],
+    'allowed_origins' => ['https://kebd-crm.vercel.app','http://localhost:7003','https://kebd.mmadinah.com'],
 
     'allowed_origins_patterns' => [],
 
